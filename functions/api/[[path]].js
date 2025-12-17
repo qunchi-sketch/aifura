0eca8525f779c0564795953507d826583c8bae9e
qunchi-sketch <qunchi1995@gmail.com>
2025-12-17 23:46:00 +0800

    Delete functions directory

--- functions/api/[[path]].js
+++
@@ -1,7 +0,0 @@
-import worker from "../idphoto-worker.js";
-
-export async function onRequest(context) {
-  // context.request: 原始请求
-  // context.env: Pages 的环境变量 + Bindings（D1/R2等）
-  return worker.fetch(context.request, context.env);
-}
